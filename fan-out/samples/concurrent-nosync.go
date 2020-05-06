f9377dbfd640e390395251b9498488d61b8bc3b0
Jamie Cuthill <jamiecuthill@flypay.co.uk>
2020-05-06 09:19:15 +0100

    Finish fan-out talk

---
+++ fan-out/samples/concurrent-nosync.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type T struct {
+	ID int
+}
+
+// START OMIT
+func main() {
+	queue := make(chan T)
+
+	n := 5
+	for i := n; i > 0; i-- {
+		go worker(queue)
+	}
+
+	fmt.Println("start")
+	for _, d := range seed(10) {
+		queue <- d
+	}
+	close(queue)
+	fmt.Println("done")
+}
+
+func emit(d T) {
+	fmt.Println("emit", d.ID)
+	time.Sleep(1 * time.Second)
+}
+
+// END OMIT
+
+func worker(queue chan T) {
+	for d := range queue {
+		emit(d)
+	}
+}
+
+func seed(n int) []T {
+	var data []T
+	for i := 1; i <= n; i++ {
+		data = append(data, T{i})
+	}
+	return data
+}
---
+++ fan-out/samples/concurrent-sync.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type T struct {
+	ID int
+}
+
+func main() {
+	queue := make(chan T)
+
+	// START OMIT
+	var wg sync.WaitGroup // HL
+
+	n := 5
+	for i := n; i > 0; i-- {
+		wg.Add(1) // HL
+		go worker(queue, &wg)
+	}
+
+	fmt.Println("start")
+	for _, d := range seed(10) {
+		queue <- d
+	}
+	close(queue)
+	wg.Wait() // HL
+	fmt.Println("done")
+	// END OMIT
+}
+
+// STARTW OMIT
+func worker(queue chan T, wg *sync.WaitGroup) {
+	defer wg.Done() // HL
+	for d := range queue {
+		emit(d)
+	}
+}
+
+// ENDW OMIT
+
+func emit(d T) {
+	fmt.Println("emit", d.ID)
+	time.Sleep(1 * time.Second)
+}
+
+func seed(n int) []T {
+	var data []T
+	for i := 1; i <= n; i++ {
+		data = append(data, T{i})
+	}
+	return data
+}
